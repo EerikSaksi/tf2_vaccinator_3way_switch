a8af2d3094dae9b734cc995c562d94a3ed8e24de
Eerik Saksi <saksi.eerik@gmail.com>
2021-03-20 13:10:39 +0200

    Slex

--- main.py
+++ main.py
@@ -8,37 +8,33 @@ class vacc_model:
     def reset(self):
         self.vaccinator = False
         self.resistance = 0
-        self.currentlySwitching = False
-    def unequip(self):
-        print("unequip")
+    def unequip(self, key):
         self.vaccinator = False
+        keyboard.press(key)
+        time.sleep(0.03)
+        keyboard.release(key)
     def cycleToResistance(self, newResistance): 
-        print('cycleToResistance({})'.format(newResistance))
-        if not self.currentlySwitching:
-            if not self.vaccinator:
-                self.currentlySwitching = True
-                keyboard.press('2')
-                time.sleep(0.015)
-                keyboard.release('2')
-                time.sleep(0.75)
-                self.vaccinator = True
-                self.currentlySwitching = False
-            while True:
-                if self.resistance == newResistance:
-                    break
-                keyboard.press('b')
-                time.sleep(0.03)
-                keyboard.release('b')
-                self.resistance += 1  
-                self.resistance %= 3
-                if self.resistance != newResistance:
-                    time.sleep(2)
+        iters = 0
+        while self.resistance != newResistance:
+            self.resistance = (self.resistance + 1) % 3
+            iters += 1
+        if not self.vaccinator:
+            self.vaccinator = True
+            keyboard.press('2')
+            time.sleep(0.015)
+            keyboard.release('2')
+            time.sleep(0.75)
+        for i in range(iters):
+            keyboard.press('b')
+            time.sleep(0.03)
+            keyboard.release('b')
+            time.sleep(0.07)
 
 vm = vacc_model()
 keyboard.on_press_key('g',  lambda x: vm.cycleToResistance(0))
 keyboard.on_press_key('F8',  lambda x: vm.cycleToResistance(1))
 keyboard.on_press_key('F7',  lambda x: vm.cycleToResistance(2))
-keyboard.on_press_key('o',  lambda x: vm.unequip())
-keyboard.on_press_key('p',  lambda x: vm.unequip())
+keyboard.on_press_key('o',  lambda x: vm.unequip("l"))
+keyboard.on_press_key('p',  lambda x: vm.unequip("k"))
 keyboard.on_press_key('6', lambda x: vm.reset())
 keyboard.wait()
